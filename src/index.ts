226d0e7a756ee0d1bce5048f736709ee29f5971b
AZM <aziyatalik4@gmail.com>
2023-04-21 10:39:11 +0900

    Remove log

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,5 @@
 import app from './app';
 
-const { name, version, homepage } = require('../package.json');
-console.log(`
-   ___  ______________  ___   ________
-  / _ |/ ___/_  __/ _ )/ _ | / __/ __/
- / __ / /__  / / / _  / __ |_\\ \\/ _/  
-/_/ |_\\___/ /_/ /____/_/ |_/___/___/ 
- 
-${name} ${version} :: Actbase Opensources.
-Contact us -> project@actbase.io
-> https://actbase.io/opensource
-> ${homepage}
-`);
-
 export const Postcode = app;
 
 export default Postcode;
